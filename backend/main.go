24ad969c615bb3653fc4728f6bcabfd1710bb00d
Mohamed Taha Bayoumi <bayoumimt@gmail.com>
2023-12-14 13:01:37 +0000

    minor change

--- backend/main.go
+++ backend/main.go
@@ -1,61 +1,9 @@
-package main
+package backend
 
 import (
-	todo "api/backend/Todo"
-	"errors"
-	"fmt"
-	"net/http"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
-
-func getTodos(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, todo.Todos)
-}
-
-func fetchTodoById(id string) (*todo.Todo, error) {
-	for i, task := range todo.Todos {
-		if task.ID == id {
-			return &todo.Todos[i], nil
-		}
-	}
-	return nil, errors.New(fmt.Sprintf("Could not find a todo item with id %s", id))
-}
-
-func getTodoById(c *gin.Context) {
-	id := c.Param("id")
-	todo, err := fetchTodoById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo item not found"})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, todo)
-}
-
-func toggleStatus(c *gin.Context) {
-	id := c.Param("id")
-	todo, err := fetchTodoById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo item not found"})
-		return
-	}
-	toggleVal := true
-	if todo.Completed {
-		toggleVal = false
-	}
-	todo.Completed = toggleVal
-	c.IndentedJSON(http.StatusOK, todo)
-}
-
-func addTodo(c *gin.Context) {
-	var newTodo todo.Todo
-	if err := c.BindJSON(&newTodo); err != nil {
-		return
-	}
-	todo.Todos = append(todo.Todos, newTodo)
-	c.IndentedJSON(http.StatusCreated, newTodo)
-}
 
 func main() {
 	router := gin.Default()
